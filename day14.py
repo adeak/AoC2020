5490ff0474a0a6d8f20807d07b3f5965d70c275f
adeak <adeak@users.noreply.github.com>
2020-12-14 18:49:18 +0100

    Day 14

--- day14.py
+++ day14.py
@@ -1,48 +1,31 @@
 from itertools import product
 
-def day14_part1(inp):
-    regs = {}
-    for row in inp.splitlines():
-        if row.startswith('mask'):
-            mask_str = row.split()[-1].lstrip('X')
-            # mask: set 1 to 1, 0 to 0
-            #       -> | with or_mask of ones, & with and_mask of zeros
-            or_mask = int(''.join(c if c == '1' else '0' for c in mask_str), 2) 
-            and_mask = int(''.join(c if c == '0' else '1' for c in mask_str), 2)
-            continue
-
-        head, *_, tail = row.split()
-        val = int(tail)
-        index = int(head[4:-1])
-
-        regs[index] = (val | or_mask) & and_mask
-
-    res = sum(regs.values())
-
-    return res
-
-    
-def day14_part2(inp):
-    # assume that filled indices fit within a reasonably-sized dict...
-
-    regs = {}
+def day14(inp, no_part2=False):
+    regs_p1 = {}
+    regs_p2 = {}
     for row in inp.splitlines():
         if row.startswith('mask'):
             mask_str = row.split()[-1]
+
+            # part 1:
+            # mask: set 1 to 1, 0 to 0
+            #       -> | with or_mask of ones, & with and_mask of zeros
             or_mask = int(''.join(c if c == '1' else '0' for c in mask_str), 2) 
+            and_mask_p1 = int(''.join(c if c == '0' else '1' for c in mask_str), 2)
+
+            # part 2:
+            # mask: set 1 to 1, X to 0 (added back later)
+            and_mask_p2 = int(''.join('0' if c == 'X' else '1' for c in mask_str), 2)
             float_powers = [exp for exp, c in enumerate(mask_str[::-1]) if c == 'X']
 
-            # handle floaters: gather every potential mask
-            or_masks = []
-            and_masks = []
+            if no_part2:
+                # first test case would be bad
+                continue
+
+            float_masks = []
             for float_bits in product(range(2), repeat=len(float_powers)):
                 float_mask = sum(bit * 2**power for bit, power in zip(float_bits, float_powers))
-                float_mask_bits = format(float_mask, '036b')
-                aux_or_mask = int(''.join(c if (c == '1' and i in float_powers) else '0' for i, c in enumerate(float_mask_bits[::-1]))[::-1], 2) 
-                aux_and_mask = int(''.join(c if (c == '0' and i in float_powers) else '1' for i, c in enumerate(float_mask_bits[::-1]))[::-1], 2) 
-
-                or_masks.append(or_mask | aux_or_mask)
-                and_masks.append(aux_and_mask)
+                float_masks.append(float_mask)
 
             continue
 
@@ -50,21 +33,29 @@ def day14_part2(inp):
         val = int(tail)
         index = int(head[4:-1])
 
-        # handle floaters:
-        for or_mask, and_mask in zip(or_masks, and_masks):
-            floated_index = (index | or_mask) & and_mask
-            regs[floated_index] = val
+        # part 1
+        regs_p1[index] = (val | or_mask) & and_mask_p1
 
-    res = sum(regs.values())
+        if no_part2:
+            # first test case would be bad
+            continue
 
-    return res
+        # part 2
+        index = (index | or_mask) & and_mask_p2
+        for float_mask in float_masks:
+            floated_index = index + float_mask
+            regs_p2[floated_index] = val
+
+    part1 = sum(regs_p1.values())
+    part2 = sum(regs_p2.values())
+
+    return part1, part2
     
 
 if __name__ == "__main__":
     testinp = open('day14.testinp').read()
-    print(day14_part1(testinp))
+    print(day14(testinp, no_part2=True)[0])
     testinp2 = open('day14.testinp2').read()
-    print(day14_part2(testinp2))
+    print(day14(testinp2)[1])
     inp = open('day14.inp').read()
-    print(day14_part1(inp))
-    print(day14_part2(inp))
+    print(day14(inp))
